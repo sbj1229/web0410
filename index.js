d4e50fe660b6345e285ffab0f39931b29a4e5f40
sbj1229 <37063431+sbj1229@users.noreply.github.com>
2018-04-10 14:27:55 +0900

    js 언더스코어 및 html 수정
    js 언더스코어를 설치 후 처리할 서버페이지를 새로 생성하여 html과 js에 처리 내용을 추가 입력
    package에 언더스코어 내용이 추가됨(설치해서)

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var _ = require('underscore');
 var express = require('express');
 var app = express();
 
@@ -17,6 +18,16 @@ app.post('/', (req, res) => {
   console.log(req.query);
   res.send('POST로 넘어온 name은 '+ req.body.name + '입니다.');
 } );
+
+// template
+app.get('/template', (req, res) => {
+  res.charset = 'utp-8';
+  var tp1 = _.template('<h1>안녕하세요? 제 이름은 <%=name%>이고 나이는 <%=age%>에요.</h1>');
+  var obj1 = { name:req.query.name, age:req.query.age};
+  console.log(req.body.name)
+  res.send(tp1(obj1));
+
+});
 
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
