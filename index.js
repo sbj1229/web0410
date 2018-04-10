07386631078fedb46dd6235e3a1e1eb02599f111
sbj1229 <37063431+sbj1229@users.noreply.github.com>
2018-04-10 13:02:42 +0900

    주석 제거 및 post body->query 수정

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.get('/', (req, res) => {
   res.send('GET으로 넘어온 name은 '+ req.query.name + '입니다.');
 } );
 
-/*
+
 app.post('/', (req, res) => {
   res.charset = 'UTF-8';
-  res.send('POST로 넘어온 name은 '+ req.body.name + '입니다.');
+  res.send('POST로 넘어온 name은 '+ req.query.name + '입니다.');
 } );
-*/
+
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
