13e24a84bccf6ef11184b11a2fba943fe876ba21
sbj1229 <37063431+sbj1229@users.noreply.github.com>
2018-04-10 13:31:49 +0900

    주석제거 및 query->body
    body 명시를 4~6줄의 코드에서 해주어 body가 인식되는듯, 4~6줄 주석일시 body인식 실패로 에러

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var app = express();
 
-// var bodyParser = require('body-parser');
-// app.use(bodyParser.json()); // support json encoded bodies
-// app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/', (req, res) => {
   res.charset = 'UTF-8';
+  console.log(req.query);
   res.send('GET으로 넘어온 name은 '+ req.query.name + '입니다.');
 } );
-
+// ()=>{} = function(){} 대치
 
 app.post('/', (req, res) => {
   res.charset = 'UTF-8';
-  res.send('POST로 넘어온 name은 '+ req.query.name + '입니다.');
+  console.log(req.query);
+  res.send('POST로 넘어온 name은 '+ req.body.name + '입니다.');
 } );
 
 
