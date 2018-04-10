6fd403ffa0340c4ac348e9cae3c50f6718b86ae7
sbj1229 <37063431+sbj1229@users.noreply.github.com>
2018-04-10 14:56:54 +0900

    체크박스 번갈아 하기 실패
    ㅁㄴㅇㄹ

--- index.js
+++ index.js
@@ -31,20 +31,37 @@ app.get('/template', (req, res) => {
 app.get('/rowcol', (req, res) => {
   res.charset = 'UTF-8';
   var checkbox = '<input name="checkbox" type="radio">';
+  var checkbox2 = '<input name="checkbox" type="checkbox" checked="checked">';
   var row = req.query.row;
   var col = req.query.col;
   var string = '';
+  var string2 ='';
   string +='<form>';
+  string2 +='<form>';
   for(var i=0; i<col;i++)
   {
+    if(i%2==0){
+      string += checkbox2;
+      string2 += checkbox;
+    }
+    else{
     string += checkbox;
+    string2 += checkbox2;
+    }
   }
   string +='</form>';
   var result ='';
   for(var i=0; i<row;i++)
   {
-    result+=string;
+      if(i%2==0){
+      result += string;
+      }
+      else{
+      result += string2;
+      }
+      result+='\n'
   }
+
   res.send(result);
 } );
 
